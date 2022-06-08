18e787797811b39504df6c22b21ea7bba0afd1b7
MilkMGN <42798255+MilkMGN@users.noreply.github.com>
2022-06-08 11:10:51 +1000

    Update main.py

--- raspi/main.py
+++ raspi/main.py
@@ -1,6 +1,9 @@
 import sacn
 # import circuitpython-nrf24l01
 import time
+import os
+
+clear = lambda: os.system('clear')
 
 # provide an IP-Address to bind to if you are using Windows and want to use multicast
 receiver = sacn.sACNreceiver()
@@ -9,10 +12,12 @@ receiver.start()  # start the receiving thread
 # define a callback function
 @receiver.listen_on('universe', universe=1)  # listens on universe 1
 def callback(packet):  # packet type: sacn.DataPacket
+    clear()
     print(packet.dmxData)  # print the received DMX data
 
 # optional: if you want to use multicast use this function with the universe as parameter
 receiver.join_multicast(1)
+
 
 time.sleep(10)  # receive for 10 seconds
 receiver.stop()
