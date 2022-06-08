2f55c170dc30f6110f8770fcfe2fa753709755ab
MilkMGN <42798255+MilkMGN@users.noreply.github.com>
2022-06-08 11:32:54 +1000

    Update main.py

--- raspi/main.py
+++ raspi/main.py
@@ -2,15 +2,32 @@ import sacn
 # import circuitpython-nrf24l01
 import time
 import os
+import configparser
+import platform
+import codecs
 
-clear = lambda: os.system('clear')
+# Import config file
+CONFIG_FILE = "./config/config.ini"
+
+config = configparser.ConfigParser()
+config.read_file(codecs.open(CONFIG_FILE, "r", "utf-8"))
+
+# set clear type
+platype = platform.system()
+if platype == "Linux":
+    clear = lambda: os.system('clear')
+elif platype == "Windows":
+    clear = lambda: os.system('cls')
+
+# Set settings based off of config file
+UNIVERSE_ID=config.get("sACN", "universe")
 
 # provide an IP-Address to bind to if you are using Windows and want to use multicast
 receiver = sacn.sACNreceiver()
 receiver.start()  # start the receiving thread
 
 # define a callback function
-@receiver.listen_on('universe', universe=1)  # listens on universe 1
+@receiver.listen_on('universe', universe=UNIVERSE_ID)  # listens on universe 1
 def callback(packet):  # packet type: sacn.DataPacket
     clear()
     print(packet.dmxData)  # print the received DMX data
