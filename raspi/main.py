5dcdc9ba145fce697bc5bda1b59e2a4acc389146
MilkMGN <42798255+MilkMGN@users.noreply.github.com>
2022-06-09 18:14:38 +1000

    Update main.py

--- raspi/main.py
+++ raspi/main.py
@@ -7,6 +7,7 @@ import configparser
 import codecs
 import platform
 import struct
+import logging
 from datetime import datetime
 
 # Import config file
@@ -17,6 +18,7 @@ config.read_file(codecs.open(CONFIG_FILE, "r", "utf-8"))
 
 # Set variable for timestamping
 timestamp = datetime.now()
+
 # set clear type
 platform = platform.system()
 if platform == "Linux":
@@ -47,7 +49,7 @@ RADIO_ADDRESS = config.get("Radio", "address")
 # RADIO_GPIO_MOSI = config.get("Radio", "gpio_mosi")
 # RADIO_GPIO_MISO = config.get("Radio", "gpio_miso")
 # RADIO_GPIO_SCK = config.get("Radio", "gpio_sck")
-RADIO_GPIO_CE = config.get("Radio", "gpio_ce")
+RADIO_GPIO_CE = int(config.get("Radio", "gpio_ce"))
 # RADIO_GPIO_CSN = config.get("Radio", "gpio_csn")
 
 def gpio_interrupt(gpio, level, tick):
@@ -65,7 +67,7 @@ def gpio_interrupt(gpio, level, tick):
     nrf.reset_packages_lost()
     nrf.power_up_rx()
 
-def init_radio(hostname: str, port: int, gpio: int, address: str) -> nrf.NRF24:
+def init_radio(hostname: str, port: int, gpio: int, address: str) -> nrf24.NRF24:
     '''Returns a NRF24 object given a valid hostname, port, gpio and address.'''
     pi = pigpio.pi(hostname, port)
     if not pi.connected:
@@ -73,7 +75,7 @@ def init_radio(hostname: str, port: int, gpio: int, address: str) -> nrf.NRF24:
 
     pi.callback(gpio, pigpio.FALLING_EDGE, gpio_interrupt)
 
-    nrf = nrf.NRF24(pi, ce=gpio, payload_size=RF24_PAYLOAD.DYNAMIC, channel=100, data_rate=RF24_DATA_RATE.RATE_250KBPS, pa_level=RF24_PA.LOW)
+    nrf = nrf24.NRF24(pi, ce=gpio, payload_size=nrf24.RF24_PAYLOAD.DYNAMIC, channel=100, data_rate=nrf24.RF24_DATA_RATE.RATE_250KBPS, pa_level=nrf24.RF24_PA.LOW)
     nrf.set_address_bytes(len(address))
     nrf.open_writing_pipe(address)
 
@@ -99,8 +101,13 @@ nrf = init_radio(RADIO_HOSTNAME, RADIO_PORT, RADIO_GPIO_CE, RADIO_ADDRESS)
 # define a callback function
 @receiver.listen_on('universe', universe=int(UNIVERSE_ID))  # listens on universe 1
 def callback(packet):  # packet type: sacn.DataPacket
-    #clear()
-    print(packet.dmxData[SHOE_CH1:SHOE_CH2 + 3] + timestamp)  # print the received DMX
+    # clear()
+    print(packet.dmxData[SHOE_CH1:SHOE_CH2 + 3], datetime.now())  # print the received DMX
+    # Set up logger to log to a file for easy reading
+    #logger = logging.getLogger()
+    #handler = logging.FileHandler('./log.log')
+    #logger.addHandler(handler)
+    #logger.error(packet.dmxData[SHOE_CH1:SHOE_CH2 + 3], datetime.now())
     # Assign the data in the appropriate channels to two tuples
     rgb_ch1 = packet.dmxData[SHOE_CH1:SHOE_CH1 + 3]
     rgb_ch2 = packet.dmxData[SHOE_CH2:SHOE_CH2 + 3]
